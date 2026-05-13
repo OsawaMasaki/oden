17d461302c6e4d5fa61e79fdbcac3a798dcd40f9
将生 大澤 <250631@jc-21.jp>
2026-05-13 12:18:44 +0900

    数値いじった

--- PlayScene.cpp
+++ PlayScene.cpp
@@ -22,20 +22,25 @@ void PlayScene::Update()
 {
 	// 動きの速さを調節するためのタイマー
 	static float timer = 0.0f;
-	timer += 0.05f; // ここを大きくすると速くなる
+	static float rot   = 0.0f;
+	timer += 0.03f; //移動する速さ
+	rot   += 0.3f;  //回る速さ
 
-	// 振幅
-	float amplitude = 2.0f;
+	// 振幅 amplitude
+	float ampx = 6.0f;
+	float ampy = 1.5f;
 
 	// 座標の設定
 	// sin関数は -1.0 〜 1.0 の間で変化
-	ot_.position_.x = sinf(timer) * amplitude;
-	ot_.position_.y = 0.0f;
-	ot_.position_.z = 0.0f;
+	float posx;
+	float posy;
+	posx = sin(timer) * ampx;
+	posy = cos(timer) * ampy;
+	ot_.position_ = { posx,posy,10.0f };
 
 	// スケールと回転
-	ot_.scale_ = { 0.5f, 0.5f, 0.5f };
-	ot_.rotate_.y += 0.1f;
+	ot_.scale_  = { 0.5f, 0.5f, 0.5f };
+	ot_.rotate_ = {0.0f, rot, 0.0f};
 }
 
 void PlayScene::Draw()
