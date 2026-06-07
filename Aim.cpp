c1d795787aac8f627e2a998140a9f78f8bfed64f
masaki osawa <250631@jc-21.jp>
2026-06-07 12:09:06 +0900

    何もしてない

--- Aim.cpp
+++ Aim.cpp
@@ -1,6 +1,7 @@
 #include "Aim.h"
 #include "Engine/Model.h"
 #include "Engine/SphereCollider.h"
+#include "Player.h"
 
 Aim::Aim(GameObject* parent)
 	:GameObject(parent, "Aim"), hModel_(-1)
@@ -12,7 +13,7 @@ void Aim::Initialize()
 	hModel_ = Model::Load("Aim.fbx");
 	assert(hModel_ >= 0);
 
-	//transform_.position_ = { transform_.position_.x,transform_.position_.y,0.0f };   //位置、ポジション
+	transform_.position_ = { transform_.position_.x,transform_.position_.y,0.0f };   //位置、ポジション
 	//transform_.scale_ = { 1.0f,1.0f,1.0f };                         //大きさ
 }
 
@@ -20,6 +21,11 @@ void Aim::Update()
 {
 	//transform_.position_.x = transform_.position_.x;
 	//transform_.position_.y = transform_.position_.y;
+	//transform_.position_.x = GetParent()->GetPosition().x;
+	//transform_.position_.y = GetParent()->GetPosition().y;
+
+	//プレイヤーの位置から照準（線を自機の先から出す）
+	Player* player = (Player*)GetParent();
 }
 
 void Aim::Draw()
